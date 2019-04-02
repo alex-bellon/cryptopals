dc8c412fd47e570bbd6b55334d5086407835b8ad
alex-bellon <alexrbellon@gmail.com>
2019-04-01 22:10:37 -0500

    Finished challenge 3 and started 4

--- set-1/challenge3.py
+++ set-1/challenge3.py
@@ -2,13 +2,41 @@ import codecs
 
 a = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
-aBin = bin(int(a, 16))[2:]
+aHex = bytes.fromhex(a)
 
 results = list()
 
-for char in range(0, 16):
-    temp = codecs.decode(hex(int(aBin, 2) ^ char), 'hex')
-    print(temp)
-    results.append(temp)
+for char in range(0, 0xff):
+    result = b''
+    for byte in aHex:
+        result += bytes([byte ^ char])
+    try:
+        results.append(result.decode('utf-8'))
+    except:
+        pass
 
-print(results)
+scores = dict()
+
+for string in results:
+    score = 0
+    for char in string:
+        if ord(char) > 64 and ord(char) < 91:
+            score += 2
+        elif ord(char) > 96 and ord(char) < 123:
+            score += 5
+        elif char is '\'' or char is ' ' or char is ',':
+            score += 2
+        else:
+            score -= 1
+    scores[string] = score
+
+high = 0
+word = ''
+
+for key in scores:
+    if scores[key] > high:
+        word = key
+        high = scores[key]
+
+print(word)
+#print(results)
